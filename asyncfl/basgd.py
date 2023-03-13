Fix gradient accumulation

asyncfl/basgd.py
@@ -4,6 +4,7 @@ import copy
 import numpy as np
 import torch
 from asyncfl import Server
+from asyncfl.network import unflatten_g
 
 
 class Buffer:
@@ -17,8 +18,14 @@ class Buffer:
         if self.N:
             # tmp_N = ((self.N - 1) / self.N)
             # print(type(((self.N - 1) / self.N)))
+
+
             for idx, (avg_g, g) in enumerate(zip(self.avg_gradient, gradient)):
-                self.avg_gradient[idx] = ((self.N - 1) / self.N) * avg_g + (1/self.N)*g.copy()
+                # Numpy version
+                # self.avg_gradient[idx] = ((self.N - 1) / self.N) * avg_g + (1/self.N)*g.copy()
+                # Torch version
+                self.avg_gradient[idx] = ((self.N - 1) / self.N) * avg_g + (1/self.N)*g.clone()
+
             #     self.avg_gradient[idx] = ((self.N - 1) / self.N) * self.avg_gradient + (1/self.N)*g.copy()
             # self.avg_gradient = [((self.N - 1) / self.N) * self.avg_gradient + (1/self.N)*g.copy() for g in gradient]
             # self.avg_gradient =  (1/self.N)*gradient
@@ -33,6 +40,35 @@ class Buffer:
 
     def __len__(self):
         return self.N
+
+class BufferSet_G:
+
+    def __init__(self, B):
+        self.buffers = []
+        self.B = B
+
+        for _b in range(B):
+            self.buffers.append(Buffer())
+
+    def receive(self, gradient, client_id):
+        b = client_id % self.B
+        self.buffers[b].add(gradient)
+
+    def nonEmpty(self):
+        # Check if all the buffers have at least 1 gradient
+        for b in self.buffers:
+            if not len(b):
+                return False
+        return True
+
+    def get_all_gradients(self) -> np.array:
+        gradients = [copy.deepcopy(b.avg_gradient) for b in self.buffers]
+        for b in self.buffers:
+            b.reset()
+
+        for x in gradients[0]:
+            np.array(x)
+        return gradients
 
 class BufferSet:
     pass
@@ -101,7 +137,13 @@ class BASGD(Server):
         #         return g
 
     def aggregate(self, gradient, worker_id=None):
-        """Update function for BASGD"""
+        """Update function for BASGD
+
+        How to rewrite this?
+        Input will be a flat numpy vector
+
+        At some point the (aggregated) vector needs to be unflattened and loaded onto the model
+        """
         # self.model.version += 1
         grad_data = gradient
         # grad_data = gradient["value"]
@@ -121,7 +163,10 @@ class BASGD(Server):
                     out.append([np.mean(x[idx], axis=0) for x in gradients])
                 return out
             # avg_grad = avg_gradients(buffer_gradients)
-            avg_grad = np.mean(buffer_gradients, axis=0)
+            # avg_grad = torch.mean(buffer_gradients, dim=1)
+            avg_grad = torch.mean(torch.stack(buffer_gradients), dim=0)
+            # avg_grad = np.mean(buffer_gradients, axis=0)
+            unflatten_g(self.network, avg_grad, self.device)
             # avg_grad = [x for x in np.mean(buffer_gradients, axis=0)]
 
             # def avg_grads(gradients):
@@ -139,8 +184,7 @@ class BASGD(Server):
             # print(f'Dim buffer_gradients={buffer_gradients.shape}')
             # print(buffer_gradients)
             # avg_grad = self.buffers.get_all_gradients()
-            model_grads = unflatten
-            self.set_gradients(avg_grad)
+            # self.set_gradients(avg_grad)
             # self.grad_history[self.model.version] = [g.copy() for g in self.model.get_gradients()]
             # self.prev_weights = {key: value.detach().clone() for key, value in self.model.get_weights().items()}
             self.optimizer.step()

asyncfl/client.py
@@ -11,7 +11,7 @@ class Client:
 
         self.pid = pid
         self.dataset_name = dataset_name
-        self.train_set, self.test_set = afl_dataset(dataset_name)
+        self.train_set, self.test_set = afl_dataset(dataset_name, use_iter=False, client_id=pid)
         # self.device = torch.device('cpu')
         self.device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
         self.network = MNIST_CNN().to(self.device)
@@ -34,49 +34,54 @@ class Client:
     def get_weights(self):
         return self.network.state_dict().copy()
 
-    def get_gradients(self):
-        # return model_gradients(self.network)
-        return self.g_flat.data.cpu().numpy()
-
-
-    # def train(self):
+    # def train(self, num_batches = -1):
     #     self.w_flat = flatten(self.network)
     #     self.g_flat = torch.zeros_like(self.w_flat)
+    #     try:
+    #         inputs, labels = next(iter(self.train_set))
+    #     except StopIteration as _si:
+    #         # Reload data
+    #         self.train_set, self.test_set = afl_dataset(self.dataset_name)
+    #         inputs, labels = next(iter(self.train_set))
+    #     # print(len(self.train_set))
+    #     inputs, labels = inputs.to(self.device), labels.to(self.device)
+    #     # print(labels)
+    #     # zero the parameter gradients
     #     self.optimizer.zero_grad()
-    #     for batch_idx, (inputs, labels) in enumerate(self.train_set):
-    #         inputs, labels = inputs.to(self.device), labels.to(self.device)
-    #         self.optimizer.zero_grad()
-    #         outputs = self.network(inputs)
-    #         loss = self.loss_function(outputs, labels)
-    #         loss.backward()
-    #         flatten_g(self.network, self.g_flat)
-    #         self.g_flat.add_(self.w_flat)
-    #         # print(self.g_flat)
-    #         self.optimizer.step()
-
-    def train(self):
-        self.w_flat = flatten(self.network)
-        self.g_flat = torch.zeros_like(self.w_flat)
-        try:
-            inputs, labels = next(self.train_set)
-        except StopIteration as _si:
-            # Reload data
-            self.train_set, self.test_set = afl_dataset(self.dataset_name)
-            inputs, labels = next(self.train_set)
-
-        inputs, labels = inputs.to(self.device), labels.to(self.device)
-        # zero the parameter gradients
-        self.optimizer.zero_grad()
-        outputs = self.network(inputs)
-        loss = self.loss_function(outputs, labels)
-        loss.backward()
-        flatten_g(self.network, self.g_flat)
-        self.g_flat.add_(self.w_flat)
-        self.optimizer.step()
+    #     outputs = self.network(inputs)
+    #     loss = self.loss_function(outputs, labels)
+    #     loss.backward()
+    #     flatten_g(self.network, self.g_flat)
+    #     # self.g_flat.add_(self.w_flat)
+    #     self.optimizer.step()
 
         # print('Finished training')
 
 
+    def train(self, num_batches = -1):
+        self.w_flat = flatten(self.network)
+        g_flat_local = torch.zeros_like(self.w_flat)
+        self.g_flat = torch.zeros_like(self.w_flat)
+        self.optimizer.zero_grad()
+        for batch_idx, (inputs, labels) in enumerate(self.train_set):
+            inputs, labels = inputs.to(self.device), labels.to(self.device)
+            self.optimizer.zero_grad()
+            outputs = self.network(inputs)
+            loss = self.loss_function(outputs, labels)
+            loss.backward()
+            flatten_g(self.network, g_flat_local)
+            self.g_flat += g_flat_local
+            # print(self.g_flat)
+            self.optimizer.step()
+            if batch_idx == num_batches:
+                break
+            # print(self.g_flat)
+
+
+
+    def get_gradients(self):
+        # return model_gradients(self.network)
+        return self.g_flat.data.cpu().numpy()
     # def train(self):
     #     for i, (inputs, labels) in enumerate(self.train_set, 0):
     #         inputs, labels = inputs.to(self.device), labels.to(self.device)

asyncfl/dataloader.py
@@ -26,13 +26,14 @@ def get_sampler(dataset, n_clients=1, rank=0, seed=-1, shuffle=True) -> SubsetRa
     if shuffle:
         np.random.shuffle(indices)
     client_indices = indices[rank:dataset_size:n_clients]
+    print(f'Sampler for client {rank}')
     return SubsetRandomSampler(client_indices)
 
 
 def load_data(data_root: str, train_batch_size: int,
               test_batch_size: int,
               client_id: int,
-              n_clients: int, seed= -1, use_iter=True) -> Tuple[Iterator[Any], Iterator[Any]]:
+              n_clients: int, seed= -1, use_iter=True) -> Tuple[DataLoader[Any], DataLoader[Any]]:
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
     )

asyncfl/network.py
@@ -44,12 +44,13 @@ def flatten_g(model, vec):
         pointer += num_param
 
 
-def unflatten_g(model, vec):
+def unflatten_g(model, vec, device:torch.device):
     pointer = 0
     for param in model.parameters():
         num_param = torch.prod(torch.LongTensor(list(param.size())))
         grad = torch.unflatten(vec[pointer:pointer + num_param], 0, param.size())
-        param.grad = grad.clone()
+
+        param.grad = grad.clone().to(device)
         # param.grad.data = vec[pointer:pointer + num_param].view(param.size())
         pointer += num_param
 

asyncfl/server.py
@@ -46,7 +46,7 @@ class Server:
         self.g_flat = torch.zeros_like(self.w_flat)
         client_gradients = client.get_gradients()
         client_gradients = torch.from_numpy(client_gradients)
-        unflatten_g(self.network, client_gradients)
+        unflatten_g(self.network, client_gradients, self.device)
         # print(f'Received update from {type(client)} with PID: {client.get_pid()}')
         self.aggregate(client_gradients, client.get_pid())
         current_model_weights = self.get_model_weights()
@@ -69,17 +69,24 @@ class Server:
         """Merges the new gradients and assigns them to their relevant parameter
         Uses the optimizer update the model based on the gradients
         @TODO: Want to replace this with a manual aggregation step
+
+
+        How to rewrite this?
+        Input will be a flat numpy vector?
         """
-        for g, p in zip(client_gradients, self.network.parameters()):
-            if g is not None:
-                try:
-                    # p.grad = torch.from_numpy(np.array(g, dtype=np.float16))
-                    p.grad = torch.from_numpy(g).to(self.device)
-                    # p.grad = g
-                except Exception as e:
-                    print('Exception')
-                    print(g)
-                    raise e
+        unflatten_g(self.network, client_gradients, self.device)
+        # print(client_gradients)
+        # for g, p in zip(client_gradients, self.network.parameters()):
+        #     if g is not None:
+        #         try:
+        #             # p.grad = torch.from_numpy(np.array(g, dtype=np.float16))
+        #             # p.grad = torch.from_numpy(g).to(self.device)
+        #             p.grad = g.to(self.device)
+        #             # p.grad = g
+        #         except Exception as e:
+        #             print('Exception')
+        #             print(g)
+        #             raise e
         self.optimizer.step()
         # for g in client_gradients:
         #     g_flat = torch.zeros_like(g)