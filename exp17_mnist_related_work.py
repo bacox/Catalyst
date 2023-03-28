31add4b64e1e4a6513443020bdd75633dff43711
bacox <bartcox93@gmail.com>
2023-03-28 22:10:38 +0200

    Reformat

--- exp17_mnist_related_work.py
+++ exp17_mnist_related_work.py
@@ -11,20 +11,28 @@ import itertools
 # Turn interactive plotting off
 plt.ioff()
 
-if __name__ == '__main__':
+
+def argument_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "-omit", help="Omit running experiment",
                         action="store_true")
     parser.add_argument('-a', '--autocomplete',
                         help="Autocomplete missing experiments. Based on the results in the datafile, missing experiment will be run.", action='store_true')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+def ensure_aux_dir_exists():
+    (data_path := Path('.data')).mkdir(exist_ok=True, parents=True)
+    (graphs_path := Path('graphs')).mkdir(exist_ok=True, parents=True)
+    return data_path, graphs_path
+
+if __name__ == '__main__':
+    args = argument_parser()
 
     print('Exp 17: Run related work in MNIST dataset. Attacks: Negative gradient (magnitude=10) '
           +'and random perbutation (alpha_atk = 0.2). Learning rate follows from learning rate sweep: exp 14.'
           +'SaSGD, BAGSD, Kardam')
 
-    (data_path := Path('.data')).mkdir(exist_ok=True, parents=True)
-    (graphs_path := Path('graphs')).mkdir(exist_ok=True, parents=True)
+    data_path, graphs_path = ensure_aux_dir_exists()
     exp_name = 'exp17_mnist_related_work_test_kardam'
     data_file = data_path / f'{exp_name}.json'
 
@@ -133,21 +141,8 @@ if __name__ == '__main__':
                 completed_runs = json.load(f)
                 keys = [x[1]['exp_id'] for x in completed_runs]
                 configs = [x for x in configs if x['exp_id'] not in keys]
-                # @TODO: Append to output instead of overwriting
 
-        # outputs = AFL.Scheduler.run_multiple(configs, pool_size=pool_size, outfile=data_file)
         AFL.Scheduler.run_multiple(configs, pool_size=pool_size, outfile=data_file, clear_file=not args.autocomplete)
-
-        # # Replace class names with strings for serialization
-        # for i in outputs:
-        #     i[1]['clients']['client'] = i[1]['clients']['client'].__name__
-        #     i[1]['clients']['f_type'] = i[1]['clients']['f_type'].__name__
-        #     i[1]['server'] = i[1]['server'].__name__
-
-        # # Write raw data to file
-        # outputs += completed_runs
-        # with open(data_file, 'w') as f:
-        #     json.dump(outputs, f)
 
     # Load raw data from file
     outputs2 = ''
@@ -221,41 +216,3 @@ if __name__ == '__main__':
     plt.ylabel('Test accuracy')
     plt.savefig(graph_file)
     plt.close(fig)
-
-    # graph_file = graphs_path / f'{exp_name}_model_age.png'
-    # print(f'Generating plot: {graph_file}')
-    # # Visualize data
-    # fig = plt.figure(figsize=(8, 6))
-    # g = sns.lineplot(data=model_age_df, x='round', y='model_age', hue='name')
-    # plt.title('Model Age')
-    # plt.xlabel('Rounds')
-    # plt.ylabel('Model age')
-    # g.legend_.set_title(None)
-    # plt.savefig(graph_file)
-    # plt.close(fig)
-
-    # graph_file = graphs_path / f'{exp_name}_model_age_hist.png'
-    # print(f'Generating plot: {graph_file}')
-    # # Visualize data
-    # fig = plt.figure(figsize=(8, 6))
-    # # g = sns.lineplot(data=model_age_df, x='round', y='model_age', hue='name')
-    # g = sns.histplot(data=model_age_df, x="model_age", kde=True, hue='name')
-    # plt.title('Model Age')
-    # plt.xlabel('Model Age')
-    # plt.ylabel('Density')
-    # # g.legend_.set_title(None)
-    # plt.savefig(graph_file)
-    # plt.close(fig)
-
-    # graph_file = graphs_path / f'{exp_name}_client_hist.png'
-    # print(f'Generating plot: {graph_file}')
-    # # Visualize data
-    # fig = plt.figure(figsize=(8, 6))
-    # # g = sns.lineplot(data=model_age_df, x='round', y='model_age', hue='name')
-    # g = sns.histplot(data=model_age_df, x="client", kde=True, hue='name')
-    # plt.title('Client contributions')
-    # plt.xlabel('Client contributions')
-    # plt.ylabel('Density')
-    # # g.legend_.set_title(None)
-    # plt.savefig(graph_file)
-    # plt.close(fig)
